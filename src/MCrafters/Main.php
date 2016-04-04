987781194e1bb79c60713f90e4cade226d8bca52
Driesboy <coenendries3@gmail.com>
2016-04-04 17:44:39 +0200

    Update Main.php

--- src/MCrafters/Main.php
+++ src/MCrafters/Main.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace Mcrafterss;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\player\PlayerChatEvent;
+class Main extends PluginBase implements Listener{
+messages:
+- fuck
+
+	public function onEnable(){
+		$this->getServer()->getLogger()->info(TextFormat::BLUE . "UChatP Has Been Enabled.");
+		$this->getServer()->getLogger()->info(TextFormat::BLUE . "By: Mcrafterss. http://github.com/MCrafterss");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		@mkdir($this->getDataFolder());
+		    $this->saveDefaultConfig();
+	}
+
+
+	public function onChatEvent(PlayerChatEvent $event){
+		$message = $event->getMessage();
+		$event->setMessage(false ? str_replace($this->config["messages"], *, $message) : str_ireplace($this->config["messages"], *, $message));
+	}
