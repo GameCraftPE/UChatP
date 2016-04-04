f19d26beeecce66f5dbc9046a7abacc09d964aa9
Driesboy <coenendries3@gmail.com>
2016-04-04 18:06:35 +0200

    fixed some bugs

--- src/MCrafters/Main.php
+++ src/MCrafters/Main.php
@@ -1,25 +1,24 @@
 <?php
-
 namespace Mcrafters;
-
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\utils\TextFormat;
 class Main extends PluginBase implements Listener{
-
 	public function onEnable(){
 		$this->getServer()->getLogger()->info(TextFormat::BLUE . "UChatP Has Been Enabled.");
 		$this->getServer()->getLogger()->info(TextFormat::BLUE . "By: Mcrafterss. http://github.com/MCrafterss");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		@mkdir($this->getDataFolder());
-		    $this->saveDefaultConfig();
+		$this->config = (new Config($this->getDataFolder()."config.yml", Config::YAML, array(
+			"messages" => array(
+				"fuck"
+			))))->getAll();
 	}
-
-
 	public function onChatEvent(PlayerChatEvent $event){
 		$message = $event->getMessage();
-		$event->setMessage(false ? str_replace($this->config["messages"], *, $message) : str_ireplace($this->config["messages"], *, $message));
+		$event->setMessage(false ? str_replace($this->config["messages"], "*", $message) : str_ireplace($this->config["messages"], "*", $message));
 	}
+}
