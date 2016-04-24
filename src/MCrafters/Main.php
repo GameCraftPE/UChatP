a87fb1563286be03fd41944caf885b6e7d00897f
Driesboy <coenendries3@gmail.com>
2016-04-24 07:53:27 +0200

    Update Main.php

--- src/MCrafters/Main.php
+++ src/MCrafters/Main.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Mcrafters;
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
@@ -7,18 +9,22 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\utils\TextFormat;
+
 class Main extends PluginBase implements Listener{
+	
 	public function onEnable(){
 		$this->getServer()->getLogger()->info(TextFormat::BLUE . "UChatP Has Been Enabled.");
 		$this->getServer()->getLogger()->info(TextFormat::BLUE . "By: Mcrafterss. http://github.com/MCrafterss");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->config = (new Config($this->getDataFolder()."config.yml", Config::YAML, array(
-			"messages" => array(
-				"fuck"
-			))))->getAll();
+		$this->saveDefaultConfig();
+		$this->mosaicList = array();
+		foreach($this->getConfig()->get("messages") as $m){
+			$this->mosaicList[] = str_repeat($this->getConfig()->get("mosaic"), strlen($m));
+		}
 	}
+	
 	public function onChatEvent(PlayerChatEvent $event){
 		$message = $event->getMessage();
-		$event->setMessage(false ? str_replace($this->config["messages"], "*", $message) : str_ireplace($this->config["messages"], "*", $message));
+		$event->setMessage($this->getConfig()->get("identify-capital-alphabet") ? str_replace($this->getConfig->get("messages"), $this->mosaicList, $message) : str_ireplace($this->getConfig->get("messages"), $this->mosaicList, $message));
 	}
 }
